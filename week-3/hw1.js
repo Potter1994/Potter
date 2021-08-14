790540f0f8b2780103e1265412ae8f64804f556c
Potter <sky753159852@gmail.com>
2021-08-15 00:39:20 +0800

    weel-3作業

---
+++ week-3/hw1.js
@@ -0,0 +1,11 @@
+function printStar(n) {
+    let arr = [];
+    let star = '';
+    if(n >= 1 && n <= 30){
+        for(i=0;i<n;i++) {
+            star += '*';
+            arr.push(console.log(star));
+          }
+    }
+
+  }
