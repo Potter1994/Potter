742c0601d7377552c63657828754106c5e2df027
Potter <sky753159852@gmail.com>
2021-12-13 03:05:30 +0800

    Todo list

--- week-21/hw1/src/App.js
+++ week-21/hw1/src/App.js
@@ -1,116 +1,203 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { TodoItem, Button } from "./components/TodoItem";
 
-const TodoWrapper = styled.div`
+const Container = styled.div`
+  // background-color: #aaa;
   display: flex;
   flex-direction: column;
-  text-align: center;
-  padding: 100px 200px;
+  align-items: center;
+  max-width: 560px;
+  margin: 150px auto 0;
+  padding: 40px;
 `;
-
-const TodoItemWrapper = styled.div`
-  border: 1px solide #d2d2d2;
-  border-radius: 12px;
-`;
-
-const FilterWrapper = styled.div`
+const TodoListWrapper = styled.div`
+  width: 100%;
+  border: 1px solid #faa;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
+`;
+const TodoListTitle = styled.div`
+  background-color: #fff;
+  text-align: center;
+  font-size: 32px;
+  font-weight: bold;
+  padding: 10px 0;
+`;
+const TodoListInputGroup = styled.div`
+  display: flex;
+  padding: 20px;
+  // background-color: #777;
   justify-content: center;
-  padding-bottom: 10px;
 `;
-
-const Title = styled.div`
-  margin: 0;
-  font-size: 36px;
-  font-weight: bolder;
+const TodoListInput = styled.input`
+  width: 200px;
+  padding: 8px;
+  &:focus {
+    outline: none;
+  }
 `;
-
-const Input = styled.input`
-  padding: 10px;
-  margin: 10px;
-  width: 50%;
-  font-size: 20px;
-  border: 1px solid #d2d2d2;
+const AddButton = styled.button`
+  padding: 8px;
+  background-color: #fff;
+  border: 1px solid #aaa;
+  border-radius: 5px;
+  margin-left: 6px;
+  cursor: pointer;
+  font-size: 18px;
+  transition: 0.2s;
+  &:hover {
+    transform: scale(1.1);
+  }
+  &:active {
+    transform: scale(1);
+  }
+`;
+const TodoItemRoot = styled.div`
+  padding: 20px;
+`;
+const TodoItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #eee;
+  margin-bottom: 5px;
+`;
+const TodoTitle = styled.h3`
+  margin-left: 10px;
+  ${(props) =>
+    props.$isDone ? "color: #aaa;text-decoration:line-through" : ""}
+`;
+const Button = styled.button`
+  padding: 6px 15px;
+  margin-right: 15px;
+  font-size: 18px;
+  cursor: pointer;
+  ${(props) => (props.$isDone ? "background-color: #000;color: #fff" : "")}
+`;
+const ButtonGroup = styled.div`
+  display: flex;
+`;
+const OptionTab = styled.div`
+  justify-content: center;
+  display: flex;
+  list-style: none;
+`;
+const Tab = styled.li`
+  border: 1px solid #faa;
+  padding: 6px 10px;
+  cursor: pointer;
+  transition: all 0.1s;
+  &:hover {
+    background-color: #aaa;
+    color: #fafafa;
+  }
+  & + li {
+    margin-left: 5px;
+  }
+  &:active {
+    transform: scale(1.1);
+  }
+  ${(props) => (props.$active ? "background-color:#faa;color:#fff;" : "")}
 `;
 
 function App() {
-  const [input, setInput] = useState("");
   const [todos, setTodos] = useState([]);
-  const [filter, setFilter] = useState("all");
+  const [todo, setTodo] = useState({ id: "", textContent: "", isDone: false });
+  const [tab, setTab] = useState("all");
 
-  const handleChangeInput = (e) => {
-    setInput(e.target.value);
+  const handleInputValue = (e) => {
+    setTodo({
+      id: new Date().getTime(),
+      textContent: e.target.value,
+      isDone: false,
+    });
   };
-
-  const addTodo = (e) => {
-    if (!input || /^s*$/.test(input)) {
-      return;
+  const handleTodoSubmit = () => {
+    if (!todo.textContent) {
+      return console.log("請輸入內容");
     }
-    setTodos([
-      { id: new Date().getTime(), text: input, isDone: false },
-      ...todos,
-    ]);
-    setInput("");
+    setTodos([todo, ...todos]);
+    setTodo("");
   };
-
-  const handleToggleIsDone = (id) => {
-    setTodos(
-      todos.map((todo) => {
-        if (todo.id !== id) return todo;
-        return {
-          ...todo,
-          isDone: !todo.isDone,
-        };
-      })
-    );
+  const handleChangeDone = (e) => {
+    const id = Number(e.target.dataset.id);
+    const newTodos = todos.map((i) => {
+      if (Number(i.id) === id) {
+        return { ...i, isDone: i.isDone ? false : true };
+      }
+      return i;
+    });
+    setTodos(newTodos);
   };
-
-  const handleDeleteTodo = (id) => {
-    setTodos(todos.filter((i) => i.id !== id));
+  const handleDeleteTodo = (e) => {
+    const id = Number(e.target.dataset.id);
+    const newTodos = todos.filter((i) => {
+      return Number(i.id) !== id;
+    });
+    setTodos(newTodos);
   };
-
   const handleKeypress = (e) => {
-    if (e.key === "Enter") return addTodo();
+    if (e.key === "Enter") {
+      handleTodoSubmit();
+    }
+    return;
   };
 
   return (
-    <TodoWrapper className="App">
-      <Title>TODO LIST</Title>
-      <div>
-        <Input
-          type="text"
-          placeholder="todo"
-          value={input}
-          onChange={handleChangeInput}
-          onKeyPress={handleKeypress}
-        />
-        <Button disabled={!input} type="submit" onClick={addTodo}>
-          Add
-        </Button>
-      </div>
-      <FilterWrapper>
-        <Button onClick={() => setFilter("all")}>全部</Button>
-        <Button onClick={() => setFilter("complete")}>已完成</Button>
-        <Button onClick={() => setFilter("unDone")}>未完成</Button>
-        <Button>清空</Button>
-      </FilterWrapper>
-      <TodoItemWrapper>
-        {/* 這邊 */}
-        {todos
-          .filter((todo) => (filter === "complete" ? todo.isDone : true))
-          .filter((todo) => (filter === "unDone" ? !todo.isDone : true))
-          .map((todo) => (
-            <TodoItem
-              key={todo.id}
-              todo={todo}
-              handleToggleIsDone={handleToggleIsDone}
-              handleDeleteTodo={handleDeleteTodo}
-            />
-          ))}
-      </TodoItemWrapper>
-    </TodoWrapper>
+    <Container>
+      <TodoListWrapper>
+        <TodoListTitle>TODO LIST</TodoListTitle>
+        <TodoListInputGroup>
+          <TodoListInput
+            onKeyPress={(e) => handleKeypress(e)}
+            value={todo.textContent || ""}
+            onChange={(e) => handleInputValue(e)}
+            type="text"
+          />
+          <AddButton onClick={() => handleTodoSubmit()}>Add Todo</AddButton>
+        </TodoListInputGroup>
+        <OptionTab onClick={(e) => setTab(e.target.dataset.tab)}>
+          <Tab $active={tab === "all"} data-tab="all">
+            全部事項
+          </Tab>
+          <Tab $active={tab === "uncomplete"} data-tab="uncomplete">
+            未完成事項
+          </Tab>
+          <Tab $active={tab === "done"} data-tab="done">
+            已完成事項
+          </Tab>
+        </OptionTab>
+        <TodoItemRoot>
+          {todos &&
+            todos
+              .filter((i) => (tab === "uncomplete" ? !i.isDone : true))
+              .filter((i) => (tab === "done" ? i.isDone : true))
+              .map((i) => (
+                <TodoItem key={i.id}>
+                  <TodoTitle $isDone={i.isDone}>{i.textContent}</TodoTitle>
+                  <ButtonGroup>
+                    <Button data-id={i.id} onClick={(e) => handleDeleteTodo(e)}>
+                      刪除
+                    </Button>
+                    <Button
+                      $isDone={i.isDone}
+                      data-id={i.id}
+                      onClick={(e) => handleChangeDone(e)}
+                    >
+                      {i.isDone ? "完成" : "未完成"}
+                    </Button>
+                  </ButtonGroup>
+                </TodoItem>
+              ))}
+        </TodoItemRoot>
+        <div style={{ display: "flex", justifyContent: "center", padding: 10 }}>
+          <button style={{ padding: 10 }} onClick={(e) => setTodos("")}>
+            清空
+          </button>
+        </div>
+      </TodoListWrapper>
+    </Container>
   );
 }
+
 export default App;
